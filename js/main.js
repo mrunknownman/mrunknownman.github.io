aeba4e1117f51b73a043fbdfde40d313300b3ef9
mrunknownman <115735603+mrunknownman@users.noreply.github.com>
2023-06-03 11:19:00 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -157,3 +157,18 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+      acc.style.backgroud = "black";
+    }
+  });
+}
